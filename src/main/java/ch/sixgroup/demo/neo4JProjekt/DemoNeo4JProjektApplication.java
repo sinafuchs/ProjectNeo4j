93bed7f5ff7c21cfb18bbe51b36ad3623f6226d7
Sina Fuchs <sinafuchs@Sinas-MacBook-Pro.local>
2020-04-17 10:29:46 +0200

    first commit neo4j project

---
+++ src/main/java/ch/sixgroup/demo/neo4JProjekt/DemoNeo4JProjektApplication.java
@@ -0,0 +1,13 @@
+package ch.sixgroup.demo.neo4JProjekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoNeo4JProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoNeo4JProjektApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ch/sixgroup/demo/neo4JProjekt/domainModels/teacher/TeacherRepository.java
@@ -0,0 +1,7 @@
+package ch.sixgroup.demo.neo4JProjekt.domainModels.teacher;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface TeacherRepository extends CrudRepository<Teacher, Integer> {
+}
